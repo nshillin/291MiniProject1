ad24c1328dd55dada69db58c2d72a0e4e0351582
Noah Shillington <nshillin@ualberta.ca>
2015-10-21 14:16:41 -0600

    Replaced Everything

--- src/LoginView.java
+++ src/LoginView.java
@@ -1,35 +1,6 @@
-import java.util.*;
-import java.io.*;
 
 public class LoginView {
-	
-    public LoginView() {
-        
-    }
-    
-	private static void main(String args[]) {
-        LoginView loginView = new LoginView();
-        loginView.clearScreen();
-		System.out.print("Username: ");
-	}	
-
-	public void clearScreen() {
-        try
-        {
-            final String os = System.getProperty("os.name");
-
-            if (os.contains("Windows"))
-            {
-                Runtime.getRuntime().exec("cls");
-            }
-            else
-            {
-                Runtime.getRuntime().exec("clear");
-            }
-        }
-        catch (final Exception e)
-        {
-            //  Handle any exceptions.
-        }
-	} 		
+	public static void main(String[] args) {
+		System.out.println("test");
+	}
 }
